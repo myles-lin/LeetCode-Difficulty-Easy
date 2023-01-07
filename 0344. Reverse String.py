ae675767b3e6b598bfe267b82a4624573ecb9fe1
myles-lin <modi311272@gmail.com>
2023-01-07 22:28:38 +0800

    modified 344

--- 0344. Reverse String.py
+++ 0344. Reverse String.py
@@ -1,3 +1,16 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        s.reverse()
+    # 1.    s.reverse()
+
+    # 2.    for i in range(len(s)//2):
+    #         # i + j == len(s) - 1
+    #         j = len(s) - 1 - i
+    #         tmp = s[i]
+    #         s[i] = s[j]
+    #         s[j] = tmp
+
+        i, j = 0, len(s) - 1
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
